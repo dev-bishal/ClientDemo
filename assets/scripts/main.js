574960d5aa70de387cfbe653602fb0ee1e55656b
dev-bishal <140333741+dev-bishal@users.noreply.github.com>
2023-12-01 00:42:07 +0530

    New Additions
    404 redirect and Newsletter Pop up

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,15 @@
 var menuBtn = document.querySelector(".menuButton");
 var responsiveMenu = document.querySelector(".mobileNav");
+var lightboxContainer = document.querySelector(".lightbox");
 
 menuBtn.addEventListener("click", ()=>{
     responsiveMenu.classList.toggle("active");
 });
+
+function triggerLightBox(component){
+    lightboxContainer.classList.toggle("hidden");
+    lightboxContainer.classList.toggle("flex");
+
+    document.querySelector(".modalContent.form").classList.add("flex");
+    document.querySelector(".modalContent.form").classList.remove("hidden");
+}
